9e7ca667e07fbdb5603a2ee68bb8f2d452f77e2d
DimPul <41774933+dimassyaiful@users.noreply.github.com>
2018-12-14 03:37:22 +0700

    rasa mencintai seorang wanita sholehah
    aa

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,11 @@
+//import { Component, OnInit } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { NavController, ModalController, AlertController, IonicModule } from '@ionic/angular';
+import 'rxjs';
+
+//provider
+import { UserService } from '../user.service';
+ 
 
 @Component({
   selector: 'app-login',
@@ -6,10 +13,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  col1: String;
+  col2: String;
 
-  constructor() { }
+  constructor(public navCtrl: NavController, public userProvider:UserService,
+    private modalCtrl:ModalController,private alertCtrl: AlertController) {}
 
   ngOnInit() {
   }
 
+  //untuk show alert
+  async alert(title,msg) {
+    const alert = await this.alertCtrl.create({
+      header: title,
+      message: msg,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+
+
+  login(){
+    console.log("Stage 1 -  Masuk Fungsi Login");
+    var data={ //array yang mau kita lempar ke db
+      "col1":this.col1,
+      "col2":this.col2
+    }
+    console.log(data);
+
+    //fungsi save
+    this.userProvider.login(data).subscribe((result)=>{
+      console.log("Stage 3 -  Berhasil Return API");
+      var return_msg: any =  result['_body']; //ambil return dari server
+      console.log(return_msg);
+      if(return_msg==1){
+        this.userProvider.session='admin';
+        this.alert('Berhasil!',"Berhasil Login");
+        this.navCtrl.navigateForward('home');
+      }else{
+        this.alert('Gagal!',"Maaf Gagal Login"); 
+        console.log(return_msg); //ini biar aja untuk debug error gagalnya
+      }
+    })
+  }
+
+  goback(){
+    this.navCtrl.goBack();
+  }
+  
 }
+
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import {Http,Response} from '@angular/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+ 
+  session:String;
+  constructor(public http: Http) {
+    console.log('Provider Login Loaded ');
+   }
+   
 
-  constructor() { }
+  login(data){
+    console.log('Stage 2 - Fungsi Login Running ');
+
+    //port nya relatif ya guys
+    var url="http://localhost/api_batamtraveller/user/login.php";
+    return this.http.post(url,data);
+  }
+
+  load(){
+    return this.http.get("http://localhost/api_batamtraveller/testcrud/load.php");
+  }
 }
