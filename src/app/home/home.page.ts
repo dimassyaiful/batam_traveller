522793566ea8afd3c38459377fedd49804de389e
DimPul <41774933+dimassyaiful@users.noreply.github.com>
2018-12-14 11:11:41 +0700

    james
    mie goreng

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { NavController, ModalController, AlertController, IonicModule } from '@ionic/angular';
+import 'rxjs';
+
+//provider
+import { TempatwisataService } from '../tempatwisata.service';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  
+  constructor(public actionSheetController: ActionSheetController, public navCtrl: NavController, public TempatWisata_Provider:TempatwisataService,
+    private modalCtrl:ModalController,private alertCtrl: AlertController) {
+      this.loaddata(); }
 
+    data_load = []; ///array untuk nampung data
+
+    ngOnInit() {
+    }
+  
+    loaddata() {
+      this.TempatWisata_Provider.load().subscribe((result) =>{
+      this.data_load= JSON.parse(result['_body']);
+      console.log(this.data_load); //untuk debugging aja
+     });
+    }
+    
+    doRefresh(event) { //untuk refresh halaman
+      console.log('Mulai Refresh');
+      this.loaddata(); //pas ditarik jalanin load data
+      setTimeout(() => {
+        console.log('Refresh Selesai');
+        event.target.complete();
+      }, 2000);
+    }
 }
